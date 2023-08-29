ea37a3913ad3a774dd665a9558ed35f94120f3f0
Abdullah-BILGEN <abdullah.abl52@gmail.com>
2023-08-29 23:11:44 +0300

    JavaScript

---
+++ assist/tutorials/frontend/external.js
@@ -0,0 +1,80 @@
+// alert("Deneme 999")
+// alert("pop-up1");
+
+// window.alert("pop-up2")
+
+// document.writeln("Sayfada göster <br/>");
+// window.document.writeln("Sayfada göster2");
+
+
+// console.log("console.log")
+// console.error("Console.log");
+// console.warn("Console.warn");
+// console.info("Console.info");
+
+
+// JS  SİNGLE THREAD bir işlem sergiler sıralı olarak biri bittiğinden diğer şlen başlar
+
+// var VARİABLE den gelmektedir 
+
+// var degiskenAdi=44;
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
+
+// var degiskenAdi55=44.23;
+// console.log(degiskenAdi55);
+// console.log(typeof degiskenAdi55);
+
+// var $degiskenAdi55="MerhabalarGüzel İnsanlar";
+// console.log($degiskenAdi55);
+// console.log(typeof $degiskenAdi55);
+
+
+// var _$degiskenAdi55=true;
+// console.log(_$degiskenAdi55);
+// console.log(typeof _$degiskenAdi55);
+
+// var _$degiskenAdi55=[];  LİST
+// var _$degiskenAdi55={};  OBJE
+
+// Hoisting
+carName="Audi";
+var carName;
+
+////////////////////////////////////////////////
+
+// let deneme=44;
+// console.log(deneme);
+// deneme=66;
+// console.log(deneme);
+
+// const deneme99=14525;
+// deneme99=66666; 
+
+////////////////////////////////////////////////carName
+
+//Operatörler
+// + _ * / % ++ --
+// &&=VE ||== VEYA  ! Değil
+let number1=12;
+let number2=5;
+
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+// = ATAMA
+// == (Türüne Bakmadan)
+// === (Türüne Bakarak)
+//////////////////////////////////////////////////////
+let userName=Number(prompt("Lütfen Sayı giriniz"));
+console.log(userName);
+
+///////////////////////////////////////////////////////
+
+// Nan
+// infinity
+// new
+// nul
