708314b5559a237b9ce7676188090d76d42201fe
Abdullah-BILGEN <abdullah.abl52@gmail.com>
2023-08-30 15:45:17 +0300

    Debud

--- assist/tutorials/frontend/external.js
+++ assist/tutorials/frontend/external.js
@@ -326,6 +326,76 @@ let conditional2 = () =>{
 }
 conditional2();
 
+// ÖDEV: 
+// Kullanıcı tarafından girilen bir sayının negatif mi, pozitif mi ? bunu yazan JS code yazınız.
+let value = Number(prompt("Bir Sayı giriniz !!"));
+function negativeOrPositive(number) {
+    result = number > 0 ? "Pozitif" : "Negatif";
+    return result;
+}
+console.log(negativeOrPositive(value));
+
+
+// Kullanıcı tarafından girilen bir sayının tek mi, çift mi olduğunu yazan JS code yazınız.
+
+let oddEvenCalculator = function () {
+    let number = prompt('Bir Sayı Giriniz');
+    if (number % 2 == 0) {// çift sayı
+        console.log(number + " Sayı iki ile bölümünden kalan 0 dır");
+    } else {
+        console.log(number + " Sayının iki ile bölümünden kalan değer mevcuttur");
+    }
+}
+oddEvenCalculator()
+
+// HOCANIN YAPTIĞI
+
+let number = Number(prompt("Bir sayı giriniz"));
+let oddEvenCalculatorTenary = number % 2 == 0 ? "Çift" : "Tek";
+console.log(oddEvenCalculatorTenary);
+
+// Kullanıcıdan alınan sayıya göre Haftanın günlerini switch-case ile JS code yazınız?
+
+let day = Number(prompt("Bir gün giriniz"));
+let weeksDay = (day) => {
+    switch (day) {
+        case 1:
+            console.log("Pazartesi");
+            break;
+
+        case 2:
+            console.log("Salı");
+            break;
+
+        case 3:
+            console.log("Çarşamba");
+            break;
+
+        case 4:
+            console.log("Perşembe");
+            break;
+
+        case 5:
+            console.log("Cuma");
+            break;
+
+        case 6:
+            console.log("Cumartesi");
+            break;
+
+        case 7:
+            console.log("Pazar");
+            break;
+
+
+        default:
+            console.log("Değer belirtilen arakıkta değil");
+            break;
+    }
+}
+
+weeksDay(day);
+
 //////////////////////////////////////////////////////
 
 // Loop
